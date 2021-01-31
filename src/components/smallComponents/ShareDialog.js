09895f21acfb5d16381641c5da3061c13b669d8c
Federica <federicucciaaa_95@hotmail.it>
2021-01-31 20:19:59 +0100

    Condivisione social aggiunta #CU-c70369

--- src/components/smallComponents/ShareDialog.js
+++ src/components/smallComponents/ShareDialog.js
@@ -1,29 +1,113 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import ShareIcon from "@material-ui/icons/Share";
-import Avatar from "@material-ui/core/Avatar";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import ListItemText from "@material-ui/core/ListItemText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
-import PersonIcon from "@material-ui/icons/Person";
-import AddIcon from "@material-ui/icons/Add";
-import { blue } from "@material-ui/core/colors";
-
-const socials = ["username@gmail.com", "user02@gmail.com"];
-const useStyles = makeStyles({
-  avatar: {
-    backgroundColor: blue[100],
-    color: blue[600],
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  LinkedinShareButton,
+  TelegramShareButton,
+  TumblrShareButton,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  TumblrIcon,
+  FacebookIcon,
+  TelegramIcon,
+  LinkedinIcon,
+  EmailIcon,
+} from "react-share";
+import { ButtonGroup } from "@material-ui/core";
+const shareUrl = "https://material-ui.com/components/dialogs/";
+const iconDimension = 40;
+const roundIcon = true;
+const socials = [
+  {
+    name: "Facebook",
+    component: (
+      <FacebookShareButton url={shareUrl} title={"Facebook"}>
+        <ButtonGroup>
+          <FacebookIcon size={iconDimension} round={roundIcon} />
+          <ListItemText primary={"Facebook"} style={{ marginLeft: "10px" }} />
+        </ButtonGroup>
+      </FacebookShareButton>
+    ),
   },
-});
-
+  {
+    name: "Whatsapp",
+    component: (
+      <WhatsappShareButton url={shareUrl} title={"Whatsapp"}>
+        <ButtonGroup>
+          <WhatsappIcon size={iconDimension} round={roundIcon} />
+          <ListItemText primary={"Whatsapp"} style={{ marginLeft: "10px" }} />
+        </ButtonGroup>
+      </WhatsappShareButton>
+    ),
+  },
+  {
+    name: "Twitter",
+    component: (
+      <TwitterShareButton url={shareUrl} title={"Twitter"}>
+        <ButtonGroup>
+          <TwitterIcon size={iconDimension} round={roundIcon} />
+          <ListItemText primary={"Twitter"} style={{ marginLeft: "10px" }} />
+        </ButtonGroup>
+      </TwitterShareButton>
+    ),
+  },
+  {
+    name: "Tumblr",
+    component: (
+      <TumblrShareButton url={shareUrl} title={"Tumblr"}>
+        <ButtonGroup>
+          <TumblrIcon size={iconDimension} round={roundIcon} />
+          <ListItemText primary={"Tumblr"} style={{ marginLeft: "10px" }} />
+        </ButtonGroup>
+      </TumblrShareButton>
+    ),
+  },
+  {
+    name: "Telegram",
+    component: (
+      <TelegramShareButton url={shareUrl} title={"Telegram"}>
+        <ButtonGroup>
+          <TelegramIcon size={iconDimension} round={roundIcon} />
+          <ListItemText primary={"Telegram"} style={{ marginLeft: "10px" }} />
+        </ButtonGroup>
+      </TelegramShareButton>
+    ),
+  },
+  {
+    name: "Linkedin",
+    component: (
+      <LinkedinShareButton url={shareUrl} title={"Linkedin"}>
+        <ButtonGroup>
+          <LinkedinIcon size={iconDimension} round={roundIcon} />
+          <ListItemText primary={"Linkedin"} style={{ marginLeft: "10px" }} />
+        </ButtonGroup>
+      </LinkedinShareButton>
+    ),
+  },
+  {
+    name: "Email",
+    component: (
+      <EmailShareButton url={shareUrl} title={"Email"}>
+        <ButtonGroup>
+          <EmailIcon size={iconDimension} round={roundIcon} />
+          <ListItemText primary={"Email"} style={{ marginLeft: "10px" }} />
+        </ButtonGroup>
+      </EmailShareButton>
+    ),
+  },
+];
 function ShareDialog(props) {
-  const classes = useStyles();
   const { onClose, selectedValue, open } = props;
 
   const handleClose = () => {
@@ -41,34 +125,17 @@ function ShareDialog(props) {
       open={open}
     >
       <DialogTitle id="simple-dialog-title">Condividi</DialogTitle>
+
       <List>
-        {socials.map((email) => (
+        {socials.map((social) => (
           <ListItem
             button
-            onClick={() => handleListItemClick(email)}
-            key={email}
+            onClick={() => handleListItemClick(social.name)}
+            key={social.name}
           >
-            <ListItemAvatar>
-              <Avatar className={classes.avatar}>
-                <PersonIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={email} />
+            <ListItemAvatar>{social.component}</ListItemAvatar>
           </ListItem>
         ))}
-
-        <ListItem
-          autoFocus
-          button
-          onClick={() => handleListItemClick("addAccount")}
-        >
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Add account" />
-        </ListItem>
       </List>
     </Dialog>
   );
